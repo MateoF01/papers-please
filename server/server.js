a70112f9957866d0e68a4d21efdc38f350a7f6a6
MateoF01 <mateofernandez380@gmail.com>
2024-11-11 17:28:07 -0300

    new domain

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const db = new sqlite3.Database('./database.db');
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://papers-please-front.onrender.com' : 'http://localhost:3000',
+    origin: process.env.NODE_ENV === 'production' ? 'https://papers-please-front-e4hv.onrender.com' : 'http://localhost:3000',
     credentials: true
 }));
 
