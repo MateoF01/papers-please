fdc79ff0d6eaf26f1ae78822280529b4e0153c10
MateoF01 <mateofernandez380@gmail.com>
2024-11-19 18:28:07 -0300

    mas headers a ver si me agarra la cookie

--- server/server.js
+++ server/server.js
@@ -35,6 +35,19 @@ app.use(cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === 'production' ? 'https://papers-please.netlify.app' : 'http://localhost:3000');
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    next();
+  });
 
 
 app.use(session({
